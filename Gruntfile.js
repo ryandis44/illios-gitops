5c5d7484ff5911dcb31b5e0d526318f4a6f8765d
Ryan D™️ <ryandisanti5810@gmail.com>
2024-12-07 18:13:10 -0600

    Squashed 'plugins/oidc-illios/' content from commit 0038ce7
    git-subtree-dir: plugins/oidc-illios
    git-subtree-split: 0038ce797a2cc8ecdfcc6e67822f43e33de2409c

---
+++ Gruntfile.js
@@ -0,0 +1,216 @@
+module.exports = function (grunt) {
+
+	require('load-grunt-tasks')(grunt);
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		composerBin: 'vendor/bin',
+
+		shell: {
+			phpcs: {
+				options: {
+					stdout: true
+				},
+				command: '<%= composerBin %>/phpcs'
+			},
+
+			phpcbf: {
+				options: {
+					stdout: true
+				},
+				command: '<%= composerBin %>/phpcbf'
+			},
+
+			phpstan: {
+				options: {
+					stdout: true
+				},
+				command: '<%= composerBin %>/phpstan analyze .'
+			},
+
+			phpunit: {
+				options: {
+					stdout: true
+				},
+				command: '<%= composerBin %>/phpunit'
+			},
+		},
+
+		gitinfo: {
+			commands: {
+				'local.tag.current.name': ['name-rev', '--tags', '--name-only', 'HEAD'],
+				'local.tag.current.nameLong': ['describe', '--tags', '--long']
+			}
+		},
+
+		clean: {
+			main: ['dist'] //Clean up build folder
+		},
+
+		copy: {
+			// Copy the plugin to a versioned release directory
+			main: {
+				src: [
+					'**',
+					'!*.xml', '!*.log', //any config/log files
+					'!node_modules/**', '!Gruntfile.js', '!package.json', '!package-lock.json', //npm/Grunt
+					'!.wordpress-org/**', //wp-org assets
+					'!dist/**', //build directory
+					'!.git/**', //version control
+					'!.github/**', //GitHub platform files
+					'!tests/**', '!scripts/**', '!phpunit.xml', '!phpunit.xml.dist', //unit testing
+					'!vendor/**', '!composer.lock', '!composer.phar', '!composer.json', //composer
+					'!wordpress/**',
+					'!.*', '!**/*~', //hidden files
+					'!CONTRIBUTING.md',
+					'!README.md',
+					'!HOWTO.md',
+					'!phpcs.xml', '!phpcs.xml.dist', '!phpstan.neon.dist', '!grumphp.yml.dist', // CodeSniffer Configuration.
+					'!docker-compose.override.yml', // Local Docker Development configuration.
+					'!codecov.yml', // Code coverage configuration.
+					'!tools/**', // Local Development/Build tools configuration.
+				],
+				dest: 'dist/',
+				options: {
+					processContentExclude: ['**/*.{png,gif,jpg,ico,mo}'],
+				},
+			}
+		},
+
+		addtextdomain: {
+			options: {
+				textdomain: 'daggerhart-openid-connect-generic',    // Project text domain.
+			},
+			update_all_domains: {
+				options: {
+					updateDomains: true
+				},
+				src: ['*.php', '**/*.php', '!node_modules/**', '!tests/**', '!tools/**', '!scripts/**', '!vendor/**', '!wp/**']
+			},
+		},
+
+		wp_readme_to_markdown: {
+			dest: {
+				files: {
+					'README.md': 'readme.txt'
+				}
+			},
+		},
+
+		makepot: {
+			target: {
+				options: {
+					domainPath: '/languages',         // Where to save the POT file.
+					exclude: [
+						'node_modules/.*',				//npm
+						'.wordpress-org/.*', 			//wp-org assets
+						'dist/.*', 								//build directory
+						'.git/.*', 								//version control
+						'.github/.*',							//GitHub platform
+						'tests/.*', 'scripts/.*',	//unit testing
+						'vendor/.*', 							//composer
+						'tools/.*'
+					],                                // List of files or directories to ignore.
+					mainFile: 'openid-connect-generic.php',                     // Main project file.
+					potFilename: 'openid-connect-generic.pot',                  // Name of the POT file.
+					potHeaders: {
+						poedit: true,                   // Includes common Poedit headers.
+						'report-msgid-bugs-to': 'https://github.com/daggerhart/openid-connect-generic/issues',
+						'x-poedit-keywordslist': true   // Include a list of all possible gettext functions.
+					},                                // Headers to add to the generated POT file.
+					type: 'wp-plugin',                // Type of project (wp-plugin or wp-theme).
+					updateTimestamp: true,            // Whether the POT-Creation-Date should be updated without other changes.
+					updatePoFiles: true               // Whether to update PO files in the same directory as the POT file.
+				}
+			}
+		},
+
+		po2mo: {
+			plugin: {
+				src: 'languages/*.po',
+				expand: true
+			}
+		},
+
+		checkrepo: {
+			deploy: {
+				tagged: true, // Check that the last commit (HEAD) is tagged
+				tag: {
+					eq: '<%= pkg.version %>' // Check if highest repo tag is equal to pkg.version
+				}
+			}
+		},
+
+		checktextdomain: {
+			options: {
+				text_domain: 'daggerhart-openid-connect-generic',
+				keywords: [
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'esc_html__:1,2d',
+					'esc_html_e:1,2d',
+					'esc_html_x:1,2c,3d',
+					'esc_attr__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'_ex:1,2c,3d',
+					'_x:1,2c,3d',
+					'_n:1,2,4d',
+					'_nx:1,2,4c,5d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d'
+				],
+			},
+			files: {
+				src: [
+					'**/*.php',
+					'!node_modules/**',
+					'!dist/**',
+					'!tests/**',
+					'!vendor/**',
+					'!tools/**',
+					'!*~',
+				],
+				expand: true,
+			},
+		},
+
+		// Bump version numbers
+		version: {
+			class: {
+				options: {
+					prefix: "const VERSION = '"
+				},
+				src: ['<%= pkg.name %>.php']
+			},
+			header: {
+				options: {
+					prefix: '\\* Version:\\s+'
+				},
+				src: ['<%= pkg.name %>.php']
+			},
+			readme: {
+				options: {
+					prefix: 'Stable tag:\\s+'
+				},
+				src: ['readme.txt']
+			}
+		}
+
+	});
+
+	grunt.registerTask('phpcs', ['shell:phpcs']);
+	grunt.registerTask('phpcbf', ['shell:phpcbf']);
+	grunt.registerTask('phpstan', ['shell:phpstan']);
+	grunt.registerTask('phpunit', ['shell:phpunit']);
+	grunt.registerTask('i18n', ['addtextdomain', 'makepot', 'po2mo']);
+	grunt.registerTask('readme', ['wp_readme_to_markdown']);
+	grunt.registerTask('test', ['checktextdomain', 'phpunit']);
+	grunt.registerTask('build', ['gitinfo', 'i18n', 'readme']);
+	grunt.registerTask('release', ['checkbranch:HEAD', 'checkrepo', 'gitinfo', 'checktextdomain', 'clean', 'copy']);
+
+};
+
---
+++ tests/phpstan-bootstrap.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Phpstan bootstrap file.
+ *
+ * @package   OpenID_Connect_Generic
+ * @author    Jonathan Daggerhart <jonathan@daggerhart.com>
+ * @author    Tim Nolte <tim.nolte@ndigitals.com>
+ * @copyright 2015-2020 daggerhart
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt GPL-2.0+
+ * @link      https://github.com/daggerhart
+ */
+
+// Define whether running under WP-CLI.
+defined( 'WP_CLI' ) || define( 'WP_CLI', false );
+
+// Define WordPress language directory.
+defined( 'WP_LANG_DIR' ) || define( 'WP_LANG_DIR', 'wordpress/src/wp-includes/languages/' );
+
+defined( 'COOKIE_DOMAIN' ) || define( 'COOKIE_DOMAIN', 'localhost' );
+defined( 'COOKIEPATH' ) || define( 'COOKIEPATH', '/');
+
+// Define Plugin Globals.
+defined( 'OIDC_CLIENT_ID' ) || define( 'OIDC_CLIENT_ID', bin2hex( random_bytes( 32 ) ) );
+defined( 'OIDC_CLIENT_SECRET' ) || define( 'OIDC_CLIENT_SECRET', bin2hex( random_bytes( 16 ) ) );
+defined( 'OIDC_ENDPOINT_LOGIN_URL' ) || define( 'OIDC_ENDPOINT_LOGIN_URL', 'https://oidc/oauth2/authorize' );
+defined( 'OIDC_ENDPOINT_USERINFO_URL' ) || define( 'OIDC_ENDPOINT_USERINFO_URL', 'https://oidc/oauth2/userinfo' );
+defined( 'OIDC_ENDPOINT_TOKEN_URL' ) || define( 'OIDC_ENDPOINT_TOKEN_URL', 'https://oidc/oauth2/token' );
+defined( 'OIDC_ENDPOINT_LOGOUT_URL' ) || define( 'OIDC_ENDPOINT_LOGOUT_URL', 'https://oidc/oauth2/logout' );
+defined( 'OIDC_CLIENT_SCOPE' ) || define( 'OIDC_CLIENT_SCOPE', 'email profile openid' );
+defined( 'OIDC_LOGIN_TYPE' ) || define( 'OIDC_LOGIN_TYPE', 'button' );
+defined( 'OIDC_LINK_EXISTING_USERS' ) || define( 'OIDC_LINK_EXISTING_USERS', 0 );
+defined( 'OIDC_ENFORCE_PRIVACY' ) || define( 'OIDC_ENFORCE_PRIVACY', 0 );
+defined( 'OIDC_CREATE_IF_DOES_NOT_EXIST' ) || define( 'OIDC_CREATE_IF_DOES_NOT_EXIST', 1 );
+defined( 'OIDC_REDIRECT_USER_BACK' ) || define( 'OIDC_REDIRECT_USER_BACK', 0 );
+defined( 'OIDC_REDIRECT_ON_LOGOUT' ) || define( 'OIDC_REDIRECT_ON_LOGOUT', 1 );
+defined( 'OIDC_ACR_VALUES' ) || define( 'OIDC_ACR_VALUES', '' );
+defined( 'OIDC_ENABLE_LOGGING' ) || define( 'OIDC_ENABLE_LOGGING', 0 );
+defined( 'OIDC_LOG_LIMIT' ) || define( 'OIDC_LOG_LIMIT', 1000 );
