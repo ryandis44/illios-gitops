6506fe68f19a633ac7f923c3eaa21c92cf9c488c
Ryan D™️ <ryandisanti5810@gmail.com>
2024-12-24 01:26:39 -0600

    fix: copy plugins to plugin folder instead of into plugins folder

--- script.sh
+++ script.sh
@@ -29,7 +29,7 @@ for file in "$HOME/gitops/wordpress/"*; do
     fi
 done
 
-mkdir -p "/var/www/html/wp-content/mu-plugins" && # Ensure mu-plugins exists
+mkdir -p "/var/www/html/wp-content/mu-plugins/" && # Ensure mu-plugins exists
 
 echo "4. Setting file/folder permissions..." && 
 # Set file permissions (primarily a post-restore step); intentionally done before plugins so they have their own permissions
