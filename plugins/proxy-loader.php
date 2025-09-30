aa082385845f99116a8ba66e023fe623fe9179d8
ryandis44 <ryandisanti5810@gmail.com>
2025-09-30 12:04:38 -0500

    add cache-illios to proxy-loader.php

--- plugins/proxy-loader.php
+++ plugins/proxy-loader.php
@@ -8,4 +8,5 @@
 
    // Require the OIDC Generic Client file
    require WPMU_PLUGIN_DIR . '/oidc-illios/openid-connect-generic.php'; // Adjust the path if needed
+   require WPMU_PLUGIN_DIR . '/cache-illios/cache-illios.php'; // Adjust the path if needed
    
