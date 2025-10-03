2c8510aa3569d2898a028f23ef0a6812f139c180
ryandis44 <ryandisanti5810@gmail.com>
2025-10-02 21:06:36 -0500

    update host-dependencies to be more descriptive

--- plugins/host-dependencies.php
+++ plugins/host-dependencies.php
@@ -1,4 +1,29 @@
 <?php
+/**
+ * Host Dependencies Loader
+ *
+ * A WordPress plugin to load required host dependencies
+ *
+ * @package   HostDependenciesLoader
+ * @category  Utility
+ * @author    Illios Digital LLC
+ * @copyright 2025 Illios Digital LLC
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Host Dependencies Loader
+ * Plugin URI:        https://github.com/ryandis44/illios-gitops
+ * Description:       Plugin to load required host dependencies like SSO and cache management.
+ * Version:           1.0.0
+ * Requires at least: 5.0
+ * Requires PHP:      7.4
+ * Author:            Illios Digital LLC
+ * Author URI:        https://illiosdigital.com
+ * Text Domain:       host-dependencies-loader
+ * Domain Path:       /languages
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
 
    // Ensure this is being run in a WordPress environment
    if ( ! defined( 'ABSPATH' ) ) {
